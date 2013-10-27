e2bf9b121762973de8265e79a002d07ad54b84d2
maximilian reuter <gentlemaxx@web.de>
2013-10-27 11:27:11 +0100

    first commit

---
+++ gpiotest.c
@@ -0,0 +1,24 @@
+#include <msp430.h> 
+
+/*
+ * 	* GPIO Test
+ *  * http://flyinventilator.wordpress.com - maximilian reuter
+ */
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+
+    P2SEL &= ~(BIT6 + BIT7);  //P2.6 and P2.7 have set a P2SEL bit initially for clock purpose. Clear before use!
+
+    //set all ports high
+    P1OUT |= BIT0+BIT1+BIT2+BIT3 +BIT4+BIT5+ BIT6 + BIT7;
+    P2OUT |= BIT0+BIT1+BIT2+BIT3 +BIT4+BIT5+ BIT6 + BIT7;
+    P3OUT |= BIT0+BIT1+BIT2+BIT3 +BIT4+BIT5+ BIT6 + BIT7;
+
+    //declare all ports output
+    P1DIR = BIT0+BIT1+BIT2+BIT3 +BIT4+BIT5+ BIT6 + BIT7;
+    P2DIR = BIT0+BIT1+BIT2+BIT3 +BIT4+BIT5 + BIT6 + BIT7;
+    P3DIR = BIT0+BIT1+BIT2+BIT3 +BIT4+BIT5+ BIT6 + BIT7;
+
+    while(1);
+	return 0;
+}
